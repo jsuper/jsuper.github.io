ec860b395c745a6babd18d93c0768aa7b8b6e2c6
Tony <ling.java@gmail.com>
2015-01-20 11:24:57 +0800

    添加多说评论系统

---
+++ static/js/common.js
@@ -0,0 +1,69 @@
+
+
+/**
+ * 将js页面的number类型转换为java的int类型
+ * @param num
+ * @return intValue
+ */
+function intValue(num)
+{
+    var MAX_VALUE = 0x7fffffff;
+    var MIN_VALUE = -0x80000000;
+    if(num > MAX_VALUE || num < MIN_VALUE)
+    {
+	return num &= 0xFFFFFFFF;
+    }
+    return num;
+}
+
+/**
+ * java String hashCode 的实现
+ * @param strKey
+ * @return intValue
+ */
+function hashCode(strKey)
+{
+    if(strKey == null || strKey.value == ""){
+        return 0 ;
+    }
+    var hash = 0;
+
+    for (var i = 0; i < strKey.length; i++)
+    {
+	hash = hash * 31 + strKey.charCodeAt(i);
+	hash = intValue(hash);
+    }
+
+    return hash;
+}
+
+function addDuoshuoPlugin(){
+    duoshuoQuery = {short_name:"ooom"};
+    (function() {
+        var ds = document.createElement('script');
+        ds.type = 'text/javascript';ds.async = true;
+        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.unstable.js';
+        ds.charset = 'UTF-8';
+        (document.getElementsByTagName('head')[0] 
+         || document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
+}
+
+
+
+$(document).ready(function(){
+    var title = $(document).find('title').text();
+    var href = window.location.href ;
+    var pathName = window.location.pathname ;
+    var isIndex = pathName === "/" || pathName === "/index.html" ;
+    var isSitemap = pathName === "/sitemap.html" ;
+    if(!isIndex && !isSitemap){
+        var id = hashCode(pathName) ;
+        var dsThread = $("#ds-thread") ;
+        dsThread.attr('data-thread-key',id);
+        dsThread.attr('data-title',title);
+        dsThread.attr('data-url',href);
+
+        addDuoshuoPlugin();
+    }
+});
